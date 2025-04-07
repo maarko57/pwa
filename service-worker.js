e6a650a35dabb1d592589fdc5e62a23b5a904e36
maarko57 <maarko@gmx.net>
2025-04-07 12:34:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            .then((registration) => {
-                console.log('Service Worker registriert mit dem Scope:', registration.scope);
-            })
-            .catch((error) => {
-                console.log('Service Worker Registrierung fehlgeschlagen:', error);
-            });
-    });
-}
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('my-pwa-cache').then((cache) => {
