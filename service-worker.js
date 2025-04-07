7ea556aca31051e32eb6a2d0bd135ff4c476c664
maarko57 <maarko@gmx.net>
2025-04-07 13:44:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v1'; // Change this to update the cache
+const CACHE_NAME = 'pwa-cache-v2'; // Change this to update the cache
 const cacheUrls = [
     '/',
     '/index.html', // Add any other HTML files you have
